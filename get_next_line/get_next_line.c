26a5374e0350f48a03eb1a8d97be4ef9c4007d4f
Sara Rudman <srudman@c2r2s6.42lisboa.com>
2023-08-11 21:30:55 +0100

    .

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: srudman <srudman@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 19:49:28 by srudman           #+#    #+#             */
-/*   Updated: 2023/07/15 17:59:01 by srudman          ###   ########.fr       */
+/*   Updated: 2023/08/11 21:09:13 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,55 +16,197 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-// size_t	ft_strlen(const char *s)
-// {
-// 	size_t	len;
-
-// 	len = 0;
-// 	while (*s)
-// 	{
-// 		s++;
-// 		len++;
-// 	}
-// 	return (len);
-// }
-
-char	*read_file(int fd)
+size_t	ft_strlen(const char *s)
 {
-	char	*buf;
-	size_t	bytes_read;
-	size_t	count;
-	size_t	buffer_size;
-	
-	if (fd == -1)
-		return(NULL);
-	buf = (char *)malloc(buffer_size + 1);
-	if (buf == NULL)
-		return (NULL);
-	count = 0;
-	buffer_size = 1000;
-	while(1)
+	size_t	len;
+
+	len = 0;
+	while (*s++)
+		len++;
+	return (len);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	ch;
+
+	ch = (char)c;
+	if (!s)
+		return (0);
+	if (ch == '\0')
+		return ((char *)s + ft_strlen(s));
+	while (*s)
 	{
-		bytes_read = read(fd, buf, buffer_size);
-		buf += bytes_read;
-		count += bytes_read;
-		if (bytes_read == buffer_size)
-			buf = (char *)malloc(10);
-		else
-		{
-			buf[count] = '\0';
-			break ;
-		}
+		if (*s == ch)
+			return ((char *)s);
+		s++;
 	}
-	*buf = *(buf - count);
-	printf("Read line: %s\n\n", buf);
-	return (buf);
+	if (ch == '\0')
+		return ((char *)s);
+	return (NULL);
 }
 
-int	main(void)
+/*
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int	fd;
-	fd = open("paragraph_file.txt", O_RDONLY);
-	read_file(fd);
-	return(0);
+	char	*ptr;
+	size_t	i;
+
+	if (!s1 || !s2)
+		return (NULL);
+	ptr = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1)); 
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		ptr[i++] = *s1++;
+	while (*s2 && *s2 != '\n')
+		ptr[i++] = *s2++;
+	ptr[i] = '\0';
+	return (ptr);
 }
+*/
+
+/* study this */
+char	*ft_strjoin_modified(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	c;
+	char	*str;
+
+	if (!s1)
+	{
+		s1 = (char *)malloc(1 * sizeof(char));
+		s1[0] = '\0';
+	}
+	if (!s1 || !s2)
+		return (NULL);
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	c = 0;
+	if (s1)
+		while (s1[++i] != '\0')
+			str[i] = s1[i];
+	while (s2[c] != '\0')
+		str[i++] = s2[c++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (str);
+}
+
+char	*ft_keep_text_remainder(char *text)
+{
+	char	*remainder;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (text[i] && text[i] != '\n')
+		i++;
+	if (!text[i])
+	{
+		free(text);
+		return (NULL);
+	}
+	remainder = (char *)malloc((ft_strlen(text) - i + 1) * sizeof(char));
+	if (!remainder)
+		return (NULL);
+	j = 0;
+	while (text[i])
+	{
+		remainder[j++] = text[++i];
+	}
+	remainder[j] = '\0';
+	free (text);
+	return (remainder);
+}
+
+char	*ft_extract_line(char *text)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!text[i])
+		return (NULL);
+	while (text[i] && text[i] != '\n')
+		i++;
+	line = (char *)malloc((i + 2) * sizeof(char));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (text[i] && text[i] != '\n')
+	{
+		line[i] = text[i];
+		i++;
+	}
+	if (text[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_get_text(int fd, char *text)
+{
+	char	*buff;
+	int		bytes_read;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	bytes_read = 1;
+	while (!ft_strchr(text, '\n') && bytes_read != 0)
+	{
+		bytes_read = read(fd, buff, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[bytes_read] = '\0';
+		text = ft_strjoin_modified(text, buff);
+	}
+	free(buff);
+	return (text);
+}
+
+/* In this function I check if fd is okay. 
+Then I read from the file and save what was read in variable "text".
+I extract line from the text and return it.
+But before I return the linke I altert the variable "text" again so 
+I only keep the remainer of the text to be used the next time the 
+function is called.  */
+char	*get_next_line(int fd)
+{
+	static char	*text;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	text = ft_get_text(fd, text);
+	if (!text)
+		return (NULL);
+	line = ft_extract_line(text);
+	text = ft_keep_text_remainder(text);
+	return (line);
+}
+
+// int	main(void)
+// {
+// 	int	fd;
+// 	char *line;
+
+// 	line = malloc(1);
+// 	fd = open("read_error.txt", O_RDONLY);
+
+// 	while (line)
+// 	{
+// 		free(line);
+// 		line = get_next_line(fd);
+// 		printf("%s", line);
+// 	}
+// 	close(fd);
+// 	return (0);
+// }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: srudman <srudman@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 19:46:13 by srudman           #+#    #+#             */
-/*   Updated: 2023/07/14 17:57:38 by srudman          ###   ########.fr       */
+/*   Updated: 2023/08/11 21:28:52 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,11 @@
 // fcntl.h needed for flags in the open() function
 
 char	*get_next_line(int fd);
-
-typedef struct s_stash
-{
-	char	*str; 
-}	t_stash;
+char	*ft_get_text(int fd, char *text);
+char	*ft_extract_line(char *text);
+char	*ft_keep_text_remainder(char *text);
+char	*ft_strjoin_modified(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
 
 #endif
