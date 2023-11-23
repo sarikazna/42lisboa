fc1e50a2829e3624358e70595d6b6a5beba22eb0
Sara Rudman <srudman@c1r16s7.42lisboa.com>
2023-11-23 17:10:49 +0000

    working version of push_swap only

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,8 +6,20 @@
 /*   By: srudman <srudman@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 14:42:01 by srudman           #+#    #+#             */
-/*   Updated: 2023/11/23 14:42:17 by srudman          ###   ########.fr       */
+/*   Updated: 2023/11/23 16:43:43 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc <= 1)
+	{
+		//check if input is correct, still needs some owork
+		// 
+		ft_printf("You have to introduce at least one argument");
+		return (1);
+	}
+	return (0);
+}
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: srudman <srudman@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 14:37:29 by srudman           #+#    #+#             */
-/*   Updated: 2023/11/23 14:38:47 by srudman          ###   ########.fr       */
+/*   Updated: 2023/11/23 17:10:20 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,14 @@
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct	s_stack_node
+{
+	int		value;
+	int		position;
+	struct s_stack_node *prev;
+	struct s_stack_node *next;
+	struct s_stack_node
+	
+}				s_stack_node;
 
 #endif
