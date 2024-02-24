7a3bd4b0a1a1ae27d024b8ba3325b0ded03003f3
Sara Rudman <srudman@c2r1s6.42lisboa.com>
2024-02-24 19:25:06 +0000

    .

--- so_long/src/validate_map2.c
+++ so_long/src/validate_map2.c
@@ -6,11 +6,33 @@
 /*   By: srudman <srudman@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 17:50:12 by srudman           #+#    #+#             */
-/*   Updated: 2024/02/24 18:42:45 by srudman          ###   ########.fr       */
+/*   Updated: 2024/02/24 19:24:32 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/so_long.h"
+
+/* 
+Still needs work
+*/
+int	ft_flood_map_to_exit(int x, int y, char **matrix)
+{
+	if (matrix[y][x] == 'E')
+		return (1); // exit was successfully found
+	matrix[y][x] = 'V'; // 'V' represents visited
+	if (matrix[y][x] == '0' || matrix[y][x] == 'C')
+	{
+		if (matrix[y + 1][x] == '0' || matrix[y + 1][x] == 'C' || matrix[y + 1][x] == 'E')
+			ft_flood_map_to_exit(y + 1, x, matrix);
+		if (matrix[y - 1][x] == '0' || matrix[y - 1][x] == 'C' || matrix[y - 1][x] == 'E')
+			ft_flood_map_to_exit(y - 1, x, matrix);
+		if (matrix[y][x + 1] == '0' || matrix[y][x + 1] == 'C' || matrix[y][x + 1] == 'E')
+			ft_flood_map_to_exit(y, x + 1, matrix);
+		if (matrix[y][x - 1] == '0' || matrix[y][x - 1] == 'C' || matrix[y][x - 1] == 'E')
+			ft_flood_map_to_exit(y, x - 1, matrix);
+	}
+	return (0);
+}
 
 void	ft_assign_position(t_map_data *map)
 {
@@ -44,7 +66,11 @@ start node by a path of the target color and changes them to the replacement col
 int ft_validate_path(t_map_data *map)
 {
 	ft_assign_position(map);
-	//ft_flood_map();
+	if (!ft_flood_map_to_exit(map->player_posX, map->player_posY, map->matrix))
+	{
+		perror("Error\nExit cannot be reached.\n");
+		return (0);	
+	}
 	return (1);
 }
 
