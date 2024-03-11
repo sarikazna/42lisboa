c3653d694029ee548995a9c592c5d1990075cbd8
srudman <sarika.zna@gmail.com>
2024-03-11 20:10:04 +0000

    last steps

--- so_long/src/movements.c
+++ so_long/src/movements.c
@@ -6,7 +6,7 @@
 /*   By: srudman <srudman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 15:39:27 by srudman           #+#    #+#             */
-/*   Updated: 2024/03/11 17:40:53 by srudman          ###   ########.fr       */
+/*   Updated: 2024/03/11 19:13:00 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,48 +14,84 @@
 
 void	move_up(t_map_data *map)
 {
-	if (map->matrix[map->player_posY - 1][map->player_posX] == '0')
+	if (map->matrix[map->player_posY - 1][map->player_posX] == '0' ||
+		map->matrix[map->player_posY - 1][map->player_posX] == 'C')
 	{
+		if (map->matrix[map->player_posY - 1][map->player_posX] == 'C')
+			map->curr_score++;
 		map->matrix[map->player_posY][map->player_posX] = '0';
 		map->matrix[map->player_posY - 1][map->player_posX] = 'P';
 		put_image(map, map->img[0], map->player_posX * 64, map->player_posY * 64);
-		put_image(map, map->img[2], map->player_posX * 64, (map->player_posY  - 1) * 64);
+		put_image(map, map->img[2], map->player_posX * 64, (map->player_posY - 1) * 64);
 		map->player_posY -= 1;
+	}
+	if (map->matrix[map->player_posY - 1][map->player_posX] == 'E'
+		&& map->curr_score == map->score)
+	{
+		write(1, "Congrats! You won!\n", 19);
+		exit(0);
 	}
 }
 
 void	move_down(t_map_data *map)
 {
-	if (map->matrix[map->player_posY - 1][map->player_posX] == '0')
+	if (map->matrix[map->player_posY + 1][map->player_posX] == '0' ||
+		map->matrix[map->player_posY + 1][map->player_posX] == 'C')
 	{
+		if (map->matrix[map->player_posY + 1][map->player_posX] == 'C')
+			map->curr_score++;
 		map->matrix[map->player_posY][map->player_posX] = '0';
 		map->matrix[map->player_posY + 1][map->player_posX] = 'P';
 		put_image(map, map->img[0], map->player_posX * 64, map->player_posY * 64);
 		put_image(map, map->img[2], map->player_posX * 64, (map->player_posY + 1) * 64);
 		map->player_posY += 1;
 	}
+	if (map->matrix[map->player_posY + 1][map->player_posX] == 'E'
+		&& map->curr_score == map->score)
+	{
+		write(1, "Congrats! You won!\n", 19);
+		exit(0);
+	}
 }
 
 void	move_left(t_map_data *map)
 {
-	if (map->matrix[map->player_posY][map->player_posX - 1] == '0')
+	if (map->matrix[map->player_posY][map->player_posX - 1] == '0' ||
+		map->matrix[map->player_posY][map->player_posX - 1] == 'C')
 	{
+		if (map->matrix[map->player_posY][map->player_posX - 1] == 'C')
+			map->curr_score++;
 		map->matrix[map->player_posY][map->player_posX] = '0';
 		map->matrix[map->player_posY][map->player_posX - 1] = 'P';
 		put_image(map, map->img[0], map->player_posX * 64, map->player_posY * 64);
 		put_image(map, map->img[2], (map->player_posX - 1) * 64, map->player_posY * 64);
 		map->player_posX -= 1;
 	}
+	if (map->matrix[map->player_posY][map->player_posX - 1] == 'E'
+		&& map->curr_score == map->score)
+	{
+		write(1, "Congrats! You won!\n", 19);
+		exit(0);
+	}
 }
 
 void	move_right(t_map_data *map)
 {
-	if (map->matrix[map->player_posY][map->player_posX + 1] == '0')
+	if (map->matrix[map->player_posY][map->player_posX + 1] == '0' ||
+		map->matrix[map->player_posY][map->player_posX + 1] == 'C')
+		if (map->matrix[map->player_posY][map->player_posX + 1] == 'C')
+			map->curr_score++;
 	{
 		map->matrix[map->player_posY][map->player_posX] = '0';
 		map->matrix[map->player_posY][map->player_posX + 1] = 'P';
 		put_image(map, map->img[0], map->player_posX * 64, map->player_posY * 64);
 		put_image(map, map->img[2], (map->player_posX + 1) * 64, map->player_posY * 64);
 		map->player_posX += 1;
+	}
+	if (map->matrix[map->player_posY][map->player_posX + 1] == 'E'
+		&& map->curr_score == map->score)
+	{
+		write(1, "Congrats! You won!\n", 19);
+		exit(0);
 	}
 }
--- so_long/src/play_game.c
+++ so_long/src/play_game.c
@@ -6,7 +6,7 @@
 /*   By: srudman <srudman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 19:00:25 by srudman           #+#    #+#             */
-/*   Updated: 2024/03/11 17:43:05 by srudman          ###   ########.fr       */
+/*   Updated: 2024/03/11 18:57:38 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,7 @@ int	move_is_valid(t_map_data *map, int x, int y)
 	else if (map->matrix[y][x] == 'E' && map->curr_score == map->score)
 		return (1);
 	else
-	{
-		printf("invalid move\n");
 		return (0);
-	}
 }
 
 /* This function is called by 
@@ -51,25 +48,13 @@ int	handle_input(int keysym, t_map_data **map)
 		exit(0);	
 	}
 	if (keysym == KEY_W && move_is_valid(*map, (*map)->player_posX, ((*map)->player_posY - 1)))
-	{
 		move_up(*map);
-		return (1);
-	}
 	if (keysym == KEY_S && move_is_valid(*map, (*map)->player_posX, ((*map)->player_posY + 1)))
-	{
 		move_down(*map);
-		return (1);
-	}
 	if (keysym == KEY_A && move_is_valid(*map, ((*map)->player_posX - 1), (*map)->player_posY))
-	{
 		move_left(*map);
-		return (1);
-	}
 	if (keysym == KEY_D && move_is_valid(*map, ((*map)->player_posX + 1), (*map)->player_posY))
-	{
 		move_right(*map);
-		return (1);
-	}
 	return (0);
 }
 
@@ -81,38 +66,3 @@ int	play_game(t_map_data *map)
 	mlx_loop(map->mlx_ptr);
 	return (1);
 }
-
-
-// int	deal_key()
-// {
-// 	ft_putchar_fd('X', 1);
-// 	//mlx_pixel_put(mlx_ptr, win_ptr,);
-// 	return (0);
-// }
-
-// MEMORY LEAK
-// int on_destroy(t_map_data *map)
-// {
-// 	mlx_destroy_window(map->mlx_ptr, map->win_ptr);
-// 	mlx_destroy_display(map->mlx_ptr);
-// 	free(map->mlx_ptr);
-// 	exit(0);
-// 	return (0);
-// }
-
-// int on_keypress(int keysym, t_map_data *map)
-// {
-// 	(void)map;
-// 	printf("Pressed key: %d\\n", keysym);
-// 	return (0);
-// }
-
-/*
-	mlx_key_hook(win_ptr, deal_key, (void *)0);
-	Register key release hook
-	The KeyRelease and DestroyNotify constants are native X11 events, and can be imported through the X11 headers.
-	mlx_hook(map->win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &map);
-	Register destroy hook
-	mlx_hook(map->win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &map);
-	mlx_loop(map->mlx_ptr);
-*/
