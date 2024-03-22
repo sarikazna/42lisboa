1e31fe1b84c0d2f260c8364f8959db8f21498b1e
Sara Rudman <srudman@c2r3s2.42lisboa.com>
2024-03-22 20:02:40 +0000

    notes

--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,8 +6,17 @@
 /*   By: srudman <srudman@student.42lisboa.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 17:08:04 by srudman           #+#    #+#             */
-/*   Updated: 2024/03/22 17:08:13 by srudman          ###   ########.fr       */
+/*   Updated: 2024/03/22 19:58:07 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pipex.h"
+
+int main(int argc, char **argv, char **envp)
+{
+    // Check if the number of arguments is correct
+    // Check if the input file exists
+    // Check if the output file is writable or can be created
+    // Check if a pipe can be created
+
+}
