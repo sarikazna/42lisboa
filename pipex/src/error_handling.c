937139fb2f571a9db6aa8959b08da9b030dc560b
srudman <sarika.zna@gmail.com>
2024-06-03 15:38:59 +0100

    .

--- pipex/src/error_handling.c
+++ pipex/src/error_handling.c
@@ -6,7 +6,7 @@
 /*   By: srudman <srudman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 18:26:57 by srudman           #+#    #+#             */
-/*   Updated: 2024/04/08 18:50:15 by srudman          ###   ########.fr       */
+/*   Updated: 2024/06/03 15:35:44 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,36 +28,33 @@ void	check_input_cmd(t_pipex_strt *data, int j, int i, int argc)
 				|| (i == argc - 2 && data->outfile_valid == false)))
 		{
 			put_error(CMD_NOT_FOUND, data->full_cmd[j]->cmd + 1);
-			// if (errno)
-			// 	perror("");
 		}
 	}
 }
 
-// deal with freaking errors later
-// problem, printf does not print into STDERR
 void	put_error(int err, char *argument)
 {
-	// Save the file descriptor for stdout
-	int stdout_fd = dup(fileno(stdout));
-	// Redirect stdout to stderr
-	dup2(fileno(stderr), fileno(stdout));
-	// Now print error messages to stderr
 	if (err == END)
 		return ;
 	else
 		ft_putstr_fd("Error: ", 2);
 	if (errno)
 	{
-		ft_printf("%s: ", argument);
+		ft_putstr_fd(argument, 2);
 		perror("");
 	}
 	else if (err == INV_ARGS)
 		ft_putstr_fd("Invalid argument\n", 2);
-	else if (err == CMD_NOT_FOUND)
-		ft_printf("%s: command not found\n", argument);
-	else if (err == NO_FILE)
-		ft_printf("%s: No such file or directory\n", argument);
+	else if (err == CMD_NOT_FOUND || err == NO_FILE || err == NO_PATH)
+	{
+		ft_putstr_fd(argument, 2);
+		if (err == CMD_NOT_FOUND)
+			ft_putstr_fd(": command not found\n", 2);
+		else if (err == NO_FILE)
+			ft_putstr_fd(": No such file or directory\n", 2);
+		else if (err == NO_PATH)
+			ft_putstr_fd(": PATH not found\n", 2);
+	}
 	else if (err == NO_MEMORY)
 		ft_putstr_fd("Memory allocation failed\n", 2);
 	else if (err == PIPE_ERR)
@@ -66,15 +63,8 @@ void	put_error(int err, char *argument)
 		ft_putstr_fd("Dup error\n", 2);
 	else if (err == FORK_ERR)
 		ft_putstr_fd("Fork failed: Cannot allocate memory\n", 2);
-	else if (err == NO_PATH)
-		ft_printf("%s: PATH not found\n", argument);
 	else if (err == CMD_FAIL)
 		ft_putstr_fd("Command failed\n", 2);
-	// Restore the original stdout
-	fflush(stdout); // Flush stdout before redirecting
-	dup2(stdout_fd, fileno(stdout));
-	// Close the duplicate file descriptor
-	close(stdout_fd);
 }
 
 void	check_files(int argc, char **argv, t_pipex_strt **data)
--- pipex/src/pipex.c
+++ pipex/src/pipex.c
@@ -6,45 +6,56 @@
 /*   By: srudman <srudman@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 20:24:19 by srudman           #+#    #+#             */
-/*   Updated: 2024/04/20 17:40:48 by srudman          ###   ########.fr       */
+/*   Updated: 2024/06/03 15:26:55 by srudman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/pipex.h"
 
-int	execute(t_cmd_strt *full_cmd, /*int *fd,*/ char **envp)
+int	execute(t_cmd_strt *full_cmd, char **envp)
 {
 	return(execve(full_cmd->cmd, full_cmd->flag, envp));
 }
 
-// void	pipex(t_pipex_strt **data, char **envp, int cmd_nbr)
-// {
-// 	int	pipefd[2];
-// 	int	status;
-// 	int	i;
+void	child_process(t_pipex_strt **data, char **envp, int pipefd[2])
+{
+	if ((*data)->infile_valid == true)
+	{
+		if (dup2((*data)->infile, STDIN_FILENO) < 0)
+			pipex_exit(*data, DUP_ERR, "dup2 failed");
+		if (dup2(pipefd[1], STDOUT_FILENO) < 0)
+			pipex_exit(*data, DUP_ERR, "dup2 failed");
+		close (pipefd[0]);
+		close ((*data)->infile);
+		execute((*data)->full_cmd[0], envp);
+	}	
+}
 
-// 	i = 0;
-// 	while (i <= cmd_nbr)
-// 	{
-// 		if (pipe(pipefd) == -1)
-// 			pipex_exit(*data, PIPE_ERR, "pipe");
-// 		if (i != cmd_nbr - 1)
-// 			execute((*data)->full_cmd[i], (*data)->infile, envp);
-// 		else
-// 			execute((*data)->full_cmd[i], (*data)->outfile, envp);
-// 		close ((*data)->infile);
-		
-		
-// 	}
-// }
-
+void	parent_process(t_pipex_strt **data, char **envp, int pipefd[2])
+{
+	pid_t	pid2;
+	
+	if ((*data)->outfile_valid == true)
+	{
+		if (dup2((*data)->outfile, STDOUT_FILENO) < 0)
+			pipex_exit(*data, DUP_ERR, "dup2 failed");
+		if (dup2(pipefd[0], STDIN_FILENO) < 0)
+			pipex_exit(*data, DUP_ERR, "dup2 failed");
+		close (pipefd[1]);
+		close ((*data)->outfile);
+		pid2 = fork();
+		if (pid2 < 0)
+			pipex_exit(*data, FORK_ERR, "Fork failed");
+		if (!pid2)
+			execute((*data)->full_cmd[1], envp);
+	}
+}
 
 void	pipex(t_pipex_strt **data, char **envp, int cmd_nbr)
 {
 	int		pipefd[2];
 	int		status;
 	pid_t	pid;
-	pid_t	pid2;
 	int		i;
 
 	if (pipe(pipefd) == -1)
@@ -53,48 +64,9 @@ void	pipex(t_pipex_strt **data, char **envp, int cmd_nbr)
 	if (pid < 0)
 		pipex_exit(*data, FORK_ERR, "Fork failed");
 	if (!pid)
-	{
-		if ((*data)->infile_valid == true)
-		{
-			if (dup2((*data)->infile, STDIN_FILENO) < 0)
-			{
-				put_error(DUP_ERR, "dup2 failed");
-				// return ;
-			}
-			if (dup2(pipefd[1], STDOUT_FILENO) < 0)
-			{
-				put_error(DUP_ERR, "dup2 failed");
-				// return ;
-			}
-			close (pipefd[0]);
-			close ((*data)->infile);
-			execute((*data)->full_cmd[0], envp);
-		}
-	}
+		child_process(data, envp, pipefd);
 	else
-	{
-		// waitpid(child pid to wait for (return value of fork), pointer to integer that stores child's exit status, 0);
-		if ((*data)->outfile_valid == true)
-		{
-			if (dup2((*data)->outfile, STDOUT_FILENO) < 0)
-			{
-				put_error(DUP_ERR, "dup2 failed");
-				// return ;
-			}
-			if (dup2(pipefd[0], STDIN_FILENO) < 0)
-			{
-				put_error(DUP_ERR, "dup2 failed");
-				// return ;
-			}
-			close (pipefd[1]);
-			close ((*data)->outfile);
-			pid2 = fork();
-			if (pid2 < 0)
-				pipex_exit(*data, FORK_ERR, "Fork failed");
-			if (!pid2)
-				execute((*data)->full_cmd[1], envp);
-		}
-	}
+		parent_process(data, envp, pipefd);
 	i = 0;
 	while (i <= cmd_nbr)
 	{
